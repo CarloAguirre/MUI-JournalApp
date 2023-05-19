6adebd88f5239d3c561e4f8577af31a5dd5e02ce
Carlo Aguirre <carlo_aguirre@outlook.cl>
2023-05-19 12:39:50 -0400

    Journal homepage advance

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,21 @@
 import { MailLockOutlined } from '@mui/icons-material'
 import { Typography } from '@mui/material'
 import React from 'react'
+import { JournalLayout } from '../layaut/JournalLayout'
 
 export const JournalPage = () => {
   return (
     <>
-      <Typography variant='h1'>JournalPage</Typography>
-      <MailLockOutlined />
+    <JournalLayout>
+      <Typography>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio iure dicta quos ipsum nam incidunt sunt, ea vitae quaerat. Quidem quas error repellendus accusantium rerum labore aut ullam beatae soluta?</Typography>
+
+      {/* NOTHINGSELECTED */}
+
+      {/* noteView */}
+
+
+    </JournalLayout>
+
     </>
   )
 }
