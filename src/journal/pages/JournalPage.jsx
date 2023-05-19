39f6870e99a9b6a8c466d41a33200af8ea759081
Carlo Aguirre <carlo_aguirre@outlook.cl>
2023-05-19 13:44:51 -0400

    views added + gallery

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,18 +1,32 @@
-import { MailLockOutlined } from '@mui/icons-material'
-import { Typography } from '@mui/material'
+import { AddOutlined, MailLockOutlined } from '@mui/icons-material'
+import { IconButton, Typography } from '@mui/material'
 import React from 'react'
 import { JournalLayout } from '../layaut/JournalLayout'
+import { NoteView, NothingSelectedView } from '../views'
 
 export const JournalPage = () => {
   return (
     <>
     <JournalLayout>
-      <Typography>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio iure dicta quos ipsum nam incidunt sunt, ea vitae quaerat. Quidem quas error repellendus accusantium rerum labore aut ullam beatae soluta?</Typography>
+      {/* <Typography>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio iure dicta quos ipsum nam incidunt sunt, ea vitae quaerat. Quidem quas error repellendus accusantium rerum labore aut ullam beatae soluta?</Typography> */}
 
-      {/* NOTHINGSELECTED */}
+      <NothingSelectedView />
 
-      {/* noteView */}
+      {/* <NoteView /> */}
 
+      <IconButton
+      size='large'
+      sx={{
+        color: 'white',
+        backgroundColor: 'error.main',
+        ':hover':{backgroundColor: 'error.main', opacity: .9},
+        position: 'fixed',
+        right: 50,
+        bottom: 50
+      }}
+      >
+        <AddOutlined sx={{fontSize: 30}}/>
+      </IconButton>
 
     </JournalLayout>
 
